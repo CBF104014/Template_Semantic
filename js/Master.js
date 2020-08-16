651b36b078ea526b0a75613a30beaff066203779
DESKTOP-J71G3OU\CAT <qwwewe56793@gmail.com>
2020-08-16 13:13:57 +0800

    Create File

---
+++ js/Master.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(() => {
+	$('#body').load('HomePage.html');
+});
+
+function Loader(url) {
+	$('#body').load(url);
+}
+
+function test() {
+	alert('just test!');
+}
+
+$('.ui.dropdown').dropdown();
