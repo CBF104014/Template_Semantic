0a56850d3b721da2f44b13ee77f3fc8934869389
DESKTOP-J71G3OU\CAT <qwwewe56793@gmail.com>
2020-08-26 00:08:43 +0800

    add i18n

--- js/Master.js
+++ js/Master.js
@@ -10,5 +10,10 @@ function test() {
 	alert('just test!');
 }
 
+var kv = new function (_key,_val) {
+	this.Key = _key;
+	this.Val = _val;
+}
+
 $('.ui.dropdown').dropdown();
 
---
+++ js/lan.js
@@ -0,0 +1,17 @@
+﻿
+var lan = {
+	tw: {
+		Shop: "商店",
+		Cart: "購物車",
+		Login: "登入",
+		Catagories: "分類",
+		Catagories1: "分類1",
+	},
+	en: {
+		Shop: "Shop",
+		Cart: "Cart",
+		Login: "Login",
+		Catagories: "Catagories",
+		Catagories1: "Catagories1",
+	}
+}
